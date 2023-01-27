0c83f9c9b9f1ccb4fa3d2ad4172325461ff5b0c6
Pholobo <ouyangphoenix@gmail.com>
2023-01-27 17:54:25 -0500

    adds loop to display weather data for next 5 days

--- develop/script.js
+++ develop/script.js
@@ -6,6 +6,7 @@ var todayIcon = document.getElementById("today-icon")
 var todayTemp = document.getElementById("today-temp");
 var todayWind = document.getElementById("today-wind");
 var todayHumidity = document.getElementById("today-humidity");
+var fiveDayContainer = document.getElementById("five-day-display");
 
 function getApi() {
     todayIcon.className = '';
@@ -30,17 +31,32 @@ function getApi() {
                     .then(function (data) {
                         console.log(data)
                         var currentCity = data.city.name;
-                        var currentDate = (data.list[0].dt_txt).substring(0,10);
-                        var currentTemp = data.list[0].main.temp;
-                        var currentWind = data.list[0].wind.speed;
-                        var currentHumidity = data.list[0].main.humidity;
-                        var currentIcon = data.list[0].weather[0].icon;
+                        var currentDate = (data.list[4].dt_txt).substring(0,10);
+                        var currentTemp = data.list[4].main.temp;
+                        var currentWind = data.list[4].wind.speed;
+                        var currentHumidity = data.list[4].main.humidity;
+                        var currentIcon = data.list[4].weather[0].icon;
 
                         todayCity.textContent = currentCity + " (" + currentDate + ")";
                         todayTemp.textContent = "Temperature: " + currentTemp + "°C";
                         todayWind.textContent = "Wind: " + currentWind + "mph";
                         todayHumidity.textContent = "Humidity: " + currentHumidity + "%";
                         todayIcon.setAttribute("src","http://openweathermap.org/img/wn/" + currentIcon + "@2x.png");
+
+                        for (var i=12; i < data.list.length; i+8) {
+                            var j = (i-12)/8;
+                            var loopDate = (data.list[i].dt_txt).substring(0,10);
+                            var loopTemp = data.list[i].main.temp;
+                            var loopWind = data.list[i].wind.speed;
+                            var loopHumidity = data.list[i].main.humidity;
+                            var loopIcon = data.list[i].weather[0].icon;
+
+                            fiveDayContainer.children[j].children[0].textContent = loopDate;
+                            fiveDayContainer.children[j].children[1].textContent = "Temp: " + loopTemp + "°C";
+                            fiveDayContainer.children[j].children[2].textContent = "Wind: " + loopWind + "mph";
+                            fiveDayContainer.children[j].children[3].textContent = "Humidity: " + loopHumidity + "%"
+                        }
+
 
                     })
         })
